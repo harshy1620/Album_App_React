f949b99e536e484b9a94e4a01861cdad2480f5dd
Harsh Yadav <harshyadav6642@gmail.com>
2023-09-06 23:53:00 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
+import Navbar from './components/navbar/nav'
+import Album from './components/Albums/album';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UpdateAlbum from './components/UpdateAlbum/UpdateAlbum';
 
 function App() {
+  const[albums,setAlbums]=useState(null);
+  
+// FETCHING THE DATA FROM THE URL
+  useEffect(() =>{
+      fetch('https://jsonplaceholder.typicode.com/albums')
+          .then((response) => response.json())
+          .then((data) => setAlbums(data))
+  },[]);
+
+
+// DELETING AN ALBUM
+const deleteAlbum = (albumId) => {
+  // showing the deletion on url
+  fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}`, {
+    method: 'DELETE',
+  })
+  .then(alert("Deleted Album successfully."));
+
+  // showing deletion on the UI
+  const newAlbums = albums.filter((album) => album.id !== albumId);
+  setAlbums(newAlbums);
+}
+  
+// ADDING AN ALBUM
+const addAlbum = (title) =>{
+  fetch('https://jsonplaceholder.typicode.com/albums', {
+method: 'POST',
+body: JSON.stringify({
+  title: title,
+  userId: 1,
+}),
+headers: {
+  'Content-type': 'application/json; charset=UTF-8',
+},
+})
+.then((response) => response.json())
+.then((json) => setAlbums([json,...albums]))
+.then(alert("Added Album Successfully"));
+};
+
+
+//updating the album
+const updateAlbum = (albumId,title) =>{
+  fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}`, {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: albumId,
+    title: title,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  const oldAlbum = albums.filter((album) => album.id.toString() === albumId);
+  oldAlbum[0].title = title;
+  alert('Update Album Successfully.')
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<Album deleteAlbum={deleteAlbum} albums={albums} addAlbum={addAlbum} />} />
+        <Route path='/update-album/:id' element={<UpdateAlbum updateAlbum={updateAlbum} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/navbar/nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./nav.css"
+const Navbar = () => {
+  return (
+    // <div ClasssName="main">
+    <nav className="container">
+    <div className="logo">
+      <img src="https://cdn-icons-png.flaticon.com/128/9437/9437982.png" alt="logo" />
+    </div>
+   <div className='heading-cont'>
+    <p>Album App</p>
+   </div>
+
+  </nav>
+ 
+  // </div>
+  )
+}
+
+export default Navbar;
